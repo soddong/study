a0da87eb53adc952ab5798c399eb0d085a98a432
최소현 SK C&C <aledjdygus9300@gmail.com>
2025-09-28 20:47:31 +0900

    feat: @Autowired 기반 의존성 주입 기능 추가
    - @Autowired 어노테이션 정의
      - 필드에 붙여 의존성을 자동 주입할 수 있도록 지원
    
    - DefaultBeanFactory 개선
      - Bean 등록 시 performInjection() 실행
      - @Autowired 필드를 탐색해 해당 타입의 Bean을 자동 주입
    
    코드 사용시,
    - Config 클래스 단순화
      - Bean 생성 시 직접 의존성 연결하지 않아도 됨
      - new SoddongService(soddongRepository()) → new SoddongService()
    
    - 서비스 코드 변경 가능
      - @Autowired 필드만 선언하면 자동으로 Repository 주입

--- soddong-spring/src/main/java/com/soddong/stdy/core/beans/factory/DefaultBeanFactory.java
+++ soddong-spring/src/main/java/com/soddong/stdy/core/beans/factory/DefaultBeanFactory.java
@@ -17,6 +17,31 @@ public class DefaultBeanFactory implements BeanFactory {
     public <T> void registerBean(Class<?> type, Object instance) {
         log.info("빈 등록: {} -> {}", type.getName(), instance);
         singletons.put(type, instance);
+        performInjection(instance);
+    }
+
+    private void performInjection(Object bean) {
+        log.debug("빈 [{}] 에 대해 @Autowired 의존성 주입 시작", bean.getClass().getName());
+
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            if (field.isAnnotationPresent(Autowired.class)) {
+                log.info("@Autowired 필드 발견: {} (대상 클래스: {})", field.getName(), bean.getClass().getName());
+                Object dependency = getBean(field.getType()); // 타입 기반 조회
+                if (dependency == null) {
+                    log.error("주입할 빈을 찾을 수 없음: {} (필드: {} in {})",
+                            field.getType().getName(), field.getName(), bean.getClass().getName());
+                    throw new RuntimeException("의존성 주입 실패: " + field);
+                }
+                try {
+                    field.setAccessible(true);
+                    field.set(bean, dependency);
+                    log.info("의존성 주입 완료: {} -> {}.{}",
+                            dependency.getClass().getName(), bean.getClass().getName(), field.getName());
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("의존성 주입 실패 " + field, e);
+                }
+            }
+        }
     }
 
     @Override
